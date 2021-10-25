f4bb0d383187bb0b1c301fa073694212d961df12
nocolour <nocolour@gmail.com>
2021-10-25 21:58:41 +0800

    Add files via upload

---
+++ udp-socket/UDPRead.py
@@ -0,0 +1,16 @@
+import socket
+
+# Create a UDP socket
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# Bind the socket to the port
+server_address = ("10.100.100.118", 3333)
+s.bind(server_address)
+print("Do Ctrl+c to exit the program !!")
+
+while True:
+    print("####### Server is listening #######")
+    data, address = s.recvfrom(4096)
+    print("\n\n 2. Server received: ", data.decode('utf-8'), "\n\n")
+#    send_data = input("Type some text to send => ")
+#    s.sendto(send_data.encode('utf-8'), address)
+#    print("\n\n 1. Server sent : ", send_data,"\n\n")
